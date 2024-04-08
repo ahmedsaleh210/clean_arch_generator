67bfefc02c3b603f092ef471e0f08eda5bf7ede7
ahmedsaleh210 <ahmed.saleh212020@gmail.com>
2024-04-08 04:38:53 +0200

    first commit

---
+++ bin/create.dart
@@ -0,0 +1,12 @@
+import 'package:args/args.dart';
+import 'package:clean_arch_generator/clean_arch_generator.dart';
+
+void main(List<String> arguments) {
+  final parser = ArgParser();
+  parser.addOption('name',
+      abbr: 'n', help: 'Name for the clean architecture feature');
+  var parsedArgs = parser.parse(arguments);
+
+  final featureName = parsedArgs['name'] as String;
+  FeatureGenerator().generateCleanFiles(featureName);
+}
